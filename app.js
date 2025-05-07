bae873af57e7c38bb30b93dcc52d883863c71a80
Ojiteli Kosisochukwu <obuekwekosi@gmail.com>
2025-05-07 23:12:19 +0100

    moved cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+app.use(cors());
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
